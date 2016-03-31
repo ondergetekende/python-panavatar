744582a4a3855eb84efd670f9f274fe11ba7afbf
Koert van der Veer <koert@ondergetekende.nl>
2016-03-31 16:39:54 +0200

    Further tinkering with cython distribution.

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from distutils.extension import Extension
+
+cmdclass = {}
+ext_modules = []
+
 try:
-    from Cython.Build import cythonize
-    ext_modules = cythonize("wallpaper/*.pyx")
+    from Cython.Distutils import build_ext
+    ext_modules += [
+        Extension("mypackage.mycythonmodule", ["cython/mycythonmodule.pyx"]),
+    ]
+    cmdclass.update({'build_ext': build_ext})
 except ImportError:
     ext_modules = []
 
 
 setup(
     name='python-wallpaper',
-    version='0.2.3',
+    version='0.2.4',
     url='https://github.com/ondergetekende/python-wallpaper',
     description=(
         'python-wallpaper generates pseudorandom abstract wallpapers'
     ),
     author='Koert van der Veer',
     author_email='koert@ondergetekende.nl',
-    packages=find_packages(),
+    packages=['wallpaper'],
     ext_modules=ext_modules,
+    cmdclass=cmdclass,
     install_requires=["cython"],
     classifiers=[
         'Intended Audience :: Developers',
