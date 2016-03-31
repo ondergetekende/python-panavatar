278e70c65a05a33afd71aee5ddec336dfa7c3968
Koert van der Veer <koert@ondergetekende.nl>
2016-03-31 11:46:35 +0200

    v0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-wallpaper',
-    version='0.1.1',
+    version='0.2.0',
     url='https://github.com/ondergetekende/python-wallpaper',
     description=(
         'python-wallpaper generates pseudorandom abstract wallpapers'
