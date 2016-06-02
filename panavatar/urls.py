6ba948b9a05181bec562e2c8e611586df013dc41
Pierre Leblanc <leblancp555@gmail.com>
2016-06-02 13:54:33 +0200

    Removed the custom part or the url to led the user choose on which url he want to call the view.

--- panavatar/urls.py
+++ panavatar/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 import panavatar.djangoview
 
 urlpatterns = [
-        url(r'^bg/(?P<width>\d+)x(?P<height>\d+).svg$',
+        url(r'^(?P<width>\d+)x(?P<height>\d+).svg$',
                     panavatar.djangoview.generate_image_svg, name='bg'),
-        url(r'^bg/(?P<width>\d+)x(?P<height>\d+)/(?P<seed>.+).svg$',
-                    panavatar.djangoview.generate_image_svg, name='bg'),
+        url(r'^(?P<width>\d+)x(?P<height>\d+)/(?P<seed>.+).svg$',
+                    panavatar.djangoview.generate_image_svg, name='bg_seed'),
 ]
